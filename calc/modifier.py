1ddbee35f924a27080ab5493aeb9371939b9c673
Scott Seligman <scott.seligman@gmail.com>
2021-04-04 12:16:50 -0700

    Start work on handling modifiers

--- calc/modifier.py
+++ calc/modifier.py
@@ -4,6 +4,12 @@ from .token import Token
 # Uses: from .value import Value
 
 class Modifier(Token):
+    table = {
+        "m": ("metric", 1),
+        "km": ("metric", 1000),
+        "cm": ("metric", .001),
+    }
+
     def __init__(self, value):
         super().__init__(value)
         
@@ -20,10 +26,39 @@ class Modifier(Token):
 
     def clone(self):
         return Modifier(self.value)
+        
+    def compatible_with(self, other):
+        if other is None:
+            return True
+        else:
+            return Modifier.table[self.value][0] == Modifier.table[other.value][0]
+    
+    def target_type(self, a, b):
+        # TODO
+        if a is None:
+            return b
+        if b is None:
+            return a
+
+        if a.value == b.value:
+            return a
+        else:
+            if Modifier.table[a.value][1] >= Modifier.table[b.value][1]:
+                return a
+            else:
+                return b
+
+    def convert_type(self, value, new_mod):
+        if value.modifier is None:
+            value.modifier = new_mod
+        else:
+            if value.modifier.value != new_mod.value:
+                value.value *= Modifier.table[new_mod.value] / Modifier.table[value.modifier.value]
+                value.modifier = new_mod
 
     @staticmethod
     def as_modifier(value):
-        if value in {"km", "m"}:
+        if value in Modifier.table:
             return Modifier(value)
         return None
-        # TODO
+
