e389fa26c4577405a019d5ff768e22a8ecc98a96
Scott Seligman <scott.seligman@gmail.com>
2021-04-05 11:24:49 -0700

    Move misc stuff into it's own folder, start adding comments

--- human_calc.py
+++ human_calc.py
@@ -5,9 +5,15 @@ import sys
 
 # Start Hide
 def test():
+    import os
+
+    # Provide several test cases and examples.
+    # For the most part order doesn't matter, but there
+    # are some test cases commented where the order matters
+
     tests = [
         ("1 + 2", "3"),
-        ("10 * last", "30"),
+        ("10 * last", "30"),  # Must be after a test that returns 3
         ("12,345 * 10", "123,450"),
         ("5+10", "15"),
         ("2 * 3", "6"),
@@ -24,11 +30,16 @@ def test():
         ("24 hours in days", "1 days"),
         ("1024 * 1024 * 1.5 bytes in mb", "1.5mb"),
         ("magic: 500 + 5 * 11", "555"),
-        ("2 * magic", "1,110"),
+        ("2 * magic", "1,110"), # Must be after a test that sets magic to 555
         ("1 btc in usd", "$57,181.50"),
     ]
+    # Just figure out how much to pad everything for display
     pad = max([len(x[0]) for x in tests])
-    engine = Calc(currency_override="currency_example.json")
+    # Note, using an engine with a hard-coded currency file so that
+    # we know what to expect for currency conversions
+    engine = Calc(currency_override=os.path.join("misc", "currency_example.json"))
+
+    # And run through all of the tests
     passed, failed = 0, 0
     for value, expected in tests:
         result = engine.calc(value)
@@ -45,43 +56,62 @@ def test():
     print(f"{passed} passed, {failed} failed")
 
     if failed > 0:
+        # If there were failures, be super verbose about it
         print("THERE WERE FAILURES")
+
+    return failed
 # End Hide        
 
 def main(test_value=None, debug=False):
     print("Human Calc:")
 
+    # There are a couple of built-in commands that run outside
+    # of the engine, we implement those has simple little local methods
+    special = {}
     def show_help():
         for key in sorted(special):
             print(f"{key:<{max([len(x) for x in special])}} = {special[key][0]}")
+    special["help"] = ("Show this help screen", show_help)
             
     def toggle_debug():
         engine.debug_mode = not engine.debug_mode
         print(f"Debug mode {'enabled' if engine.debug_mode else 'disabled'}")
+    special["debug"] = ("Enter or exit debug mode", toggle_debug)
 
-    special = {
-        "help": ("Show this help screen", show_help),
-        "debug": ("Enter or exit debug mode", toggle_debug),
-        # Start Hide
-        "test": ("Run a test", test),
-        # End Hide
-    }
+    # Start Hide
+    # This only makes sense in the full version, the test helper
+    # won't exist in the compressed version, so hide it there
+    special["test"] = ("Run a test", test)
+    # End Hide
 
+    # And grab input, and run it through the engine
     engine = Calc()
     engine.debug_mode = debug
+    if test_value is not None:
+        # As a special case, we allow multiple inputs
+        test_value = test_value.split(";")
+
     while True:
-        if test_value is None:
-            value = input()
-        else:
-            value = test_value
-        if value in special:
+        # If we're given test input, use that instead of user input
+        value = test_value.pop(0) if test_value is not None else input()
+        # If using test data, show what's happening
+        if test_value is not None:
+            print("> " + value)
+
+        if len(value) == 0:
+            # An empty input stops everything
+            break
+        elif value in special:
+            # The input was a special command, run that command
             special[value][1]()
         else:
+            # Otherwise, just run the command
             result = engine.calc(value)
-            if result is None:
-                break
+            # Crack the entire output to show the user
             print(f"= {' '.join([x.to_string() for x in result.iter()])}")
-        if test_value is not None:
+
+        if test_value is not None and len(test_value) == 0:
+            # We were given test input, don't wait for user input after
             break
 
     print("All done")
@@ -90,12 +120,16 @@ def main(test_value=None, debug=False):
 if __name__ == "__main__":
     # Start Hide
     if len(sys.argv) > 1 and sys.argv[1] == "test":
-        test()
-        exit()
+        # Run the test harness
+        exit(test())
     # End Hide
+
     if len(sys.argv) > 2 and sys.argv[1] == "run":
+        # Run the command given
         main(test_value=" ".join(sys.argv[2:]))
     elif len(sys.argv) > 2 and sys.argv[1] == "debug":
+        # Run the command given, with debug mode enabled
         main(test_value=" ".join(sys.argv[2:]), debug=True)
     else:
+        # Normal case, just ask for user input
         main()
